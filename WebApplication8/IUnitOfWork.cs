9c3272f6133dd17eb47608de91108a0328f9df48
Ekrador <101429944+Ekrador@users.noreply.github.com>
2023-08-02 13:35:23 +0500

    Добавьте файлы проекта.

---
+++ WebApplication8/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication8
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        int SaveChanges(bool ensureAutoHistory = false);
+
+        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = true) where TEntity : class;
+
+    }
+}
